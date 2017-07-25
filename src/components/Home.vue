f65c864ba207e140d347cc47676022ccfc6b54da
Yanan Lan <yananlan@YanandeMacBook-Air.local>
2017-07-25 23:52:00 +0800

    v-if v- for

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,32 @@
 <template>
-	<div class="bg">八号当铺</div>
+	<div class="bg">
+		<div v-if="seen" class="bg">八号当铺</div>
+		<div v-else class="bg">值得的人和事</div>
+		<div v-for='item in items' class='item'>{{item.name}}</div>
+	</div>	
 </template>
 
 <script>
-	export default {
-	  name: 'Home'
+export default {
+	name: 'Home',
+	data() {
+		return {
+			seen: false,	
+			items: [
+			    {name: '不值得的人和事'},
+			    {name: '不值得的钱'}]
+		}
 	}
+}
 </script>
 <style scoped>
+    .item {
+    	position: absoult;
+    	left: 0;
+    	width: 100%;
+    	height: 100px;
+    	font-size: 100px;
+    }
 	.bg{
 	  position:absoult;
 	  width: 100%;
