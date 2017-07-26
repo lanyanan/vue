c2bf86abd53f97b333671a61192e3d30c5fd28ea
Yanan Lan <yananlan@YanandeMacBook-Air.local>
2017-07-26 21:52:50 +0800

    change Ya

--- src/components/Ya.vue
+++ src/components/Ya.vue
@@ -7,3 +7,20 @@
 	  name: 'Ya'
 	}
 </script>
+<style scoped>
+    .item {
+    	position: absoult;
+    	left: 0;
+    	width: 100%;
+    	height: 100px;
+    	font-size: 100px;
+    }
+	.bg{
+	  position:absoult;
+	  width: 100%;
+	  height: 100%;
+	  background: url('../assets/bg.jpg') no-repeat left top;
+	  background-size: 100% 100%;
+	  font-size: 100px;
+    }
+</style>
